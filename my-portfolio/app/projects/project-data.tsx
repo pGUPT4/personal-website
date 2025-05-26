ae7290491f3c6804358b8d5d53c51d04d3b8962a
pGUPT4 <pratbro1@gmail.com>
2025-05-27 04:54:22 +0530

    Added more projects

--- my-portfolio/app/projects/project-data.tsx
+++ my-portfolio/app/projects/project-data.tsx
@@ -13,15 +13,28 @@ export const projects: Project[] = [
     url: "https://github.com/pGUPT4/chat-application",
   },
   {
+    title: "Credit Card Fraud Detection",
+    year: 2025,
+    description: "AI Model that predicts credit card frauds",
+    url: "https://github.com/pGUPT4/credit-card-fraud-detection",
+  },
+  {
     title: "News Application",
     year: 2025,
     description: "Get personal preferences news",
     url: "https://github.com/pGUPT4/nyt-news",
   },
   {
-    title: "Credit Card Fraud Detection",
+    title: "Android Activity-Logger",
     year: 2025,
-    description: "AI Model that predicts credit card frauds",
-    url: "https://github.com/pGUPT4/credit-card-fraud-detection",
+    description: "Store and visualize your daily activities",
+    url: "https://github.com/pGUPT4/Activity-Logger",
   },
+  {
+    title: "Escape the Spikes",
+    year: 2025,
+    description: "Pixel art game where you run away from spikes",
+    url: "https://github.com/pGUPT4/escape-the-spikes",
+  },
+  
 ];
