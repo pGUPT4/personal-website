de8a9d94f8206cbd1252dcdbd550b88d9b45f84e
pGUPT4 <pratbro1@gmail.com>
2025-06-10 10:40:57 +0530

    update project

--- my-portfolio/app/projects/project-data.tsx
+++ my-portfolio/app/projects/project-data.tsx
@@ -9,37 +9,37 @@ export const projects: Project[] = [
   {
     title: "AI ChatBot (In Progress)",
     year: 2025,
-    description: "OpenAI ChatBot with stored conversations",
+    description: "Saved Chatbot Chats",
     url: "https://github.com/pGUPT4/ai-chatbot",
   },
   {
     title: "Chat Application",
     year: 2025,
-    description: "Chat with users present on the platform",
+    description: "Connect with other users",
     url: "https://github.com/pGUPT4/chat-application",
   },
   {
     title: "Credit Card Fraud Detection",
     year: 2025,
-    description: "AI Model that predicts credit card frauds",
+    description: "Prediction AI Model",
     url: "https://github.com/pGUPT4/credit-card-fraud-detection",
   },
   {
     title: "News Application",
     year: 2025,
-    description: "Get real time news",
+    description: "Real Time News",
     url: "https://github.com/pGUPT4/nyt-news",
   },
   {
     title: "Android Activity-Logger",
     year: 2025,
-    description: "Store and visualize your daily activities",
+    description: "Track and Visualize Activities",
     url: "https://github.com/pGUPT4/Activity-Logger",
   },
   {
     title: "Escape the Spikes",
     year: 2025,
-    description: "Pixel art game where you run away from spikes",
+    description: "Pixel Art Runaway Game",
     url: "https://github.com/pGUPT4/escape-the-spikes",
   },
   
